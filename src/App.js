256643333e1be7d297f4d1ef0688a8bced4f0853
Sam <super8989@gmail.com>
2019-12-08 12:21:48 -0500

    created TopGameCategory.css

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import TopGameCategory from "./TopGameCategory";
+
 import "./App.css";
 
 function App() {
-	const [topGames, setTopGames] = useState([]);
-	const API = "https://api.twitch.tv/helix/games/top";
-	const CLIENT_ID = "mt5146rna7y3m6wlfvy2yvtq8matpa";
-
-	useEffect(() => {
-		getTwitch();
-	}, []);
-
-	console.log(topGames);
-
-	async function getTwitch() {
-		try {
-			const response = await axios.get(API, {
-				headers: { "Client-ID": CLIENT_ID }
-			});
-			setTopGames(response.data.data);
-			console.log(response);
-		} catch (error) {
-			console.error(error);
-		}
-	}
-
-	const _boxArt = url =>
-		url.replace("{width}", "300").replace("{height}", "400");
-
 	return (
 		<div className='App'>
-			{topGames.map(game => (
-				<div key={game.id}>
-					<h1>{game.name}</h1>
-					<img src={_boxArt(game.box_art_url)} alt={game.name} />
-				</div>
-			))}
+			<TopGameCategory />
 		</div>
 	);
 }
