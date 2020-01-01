097abbc6dc4daca3dacb0e28ac9bb3c4060d96b2
Sam <super8989@gmail.com>
2020-01-01 00:41:00 -0500

    added Header to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+
+import { Route, Switch, Link } from "react-router-dom";
+
+import Header from "./components/Header";
+import Games from "./components/Games";
 import TopGameCategory from "./TopGameCategory";
 
 import "./App.css";
 
 function App() {
 	return (
-		<div className='App'>
-			<TopGameCategory />
+		<div>
+			<Header />
+			<Route exact path='/' component={Games} />
 		</div>
 	);
 }
