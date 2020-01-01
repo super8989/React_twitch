716c6fd22c07e368490152a605a2bbbdda2040bf
Sam <super8989@gmail.com>
2020-01-01 01:10:13 -0500

    fetching data from twitch api to Get Top Games

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, Link } from "react-router-dom";
 
 import Header from "./components/Header";
 import Games from "./components/Games";
+import Streams from "./components/Streams";
 import TopGameCategory from "./TopGameCategory";
 
 import "./App.css";
@@ -13,6 +14,7 @@ function App() {
 		<div>
 			<Header />
 			<Route exact path='/' component={Games} />
+			<Route exact path='/top-streams' component={Streams} />
 		</div>
 	);
 }
--- src/components/Games.js
+++ src/components/Games.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../api";
 
 function Games() {
-	return <div>Games</div>;
+	const [games, setGames] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await api.get("https://api.twitch.tv/helix/games/top");
+			console.log(response.data);
+		};
+		fetchData();
+	});
+
+	return (
+		<div>
+			<h1>Most Popular Games</h1>
+		</div>
+	);
 }
 
 export default Games;
