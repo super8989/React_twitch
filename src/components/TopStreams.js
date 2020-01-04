ee89723b112cb3d572a4aa1caf43d5ef164b37b1
Sam <super8989@gmail.com>
2020-01-03 22:38:29 -0500

    added in notes and getting name of the games through 2 api calls

--- src/components/TopStreams.js
+++ src/components/TopStreams.js
@@ -6,31 +6,46 @@ function TopStreams() {
 
 	useEffect(() => {
 		const fetchData = async () => {
+			//fetch top streams
 			const response = await api.get("https://api.twitch.tv/helix/streams");
-			console.log(response.data);
 			let dataArray = response.data.data;
-			let finalArray = dataArray.map(channel => {
-				let newURL = channel.thumbnail_url
-					.replace("{width}", "300")
-					.replace("{height}", "300");
-				channel.thumbnail_url = newURL;
-				return channel;
-			});
+			console.log(response.data.data);
 
+			// array of game ids
 			let gameID = dataArray.map(stream => {
 				return stream.game_id;
 			});
-			console.log(gameID);
+			// console.log(gameID);
+
 			let baseURL = `https://api.twitch.tv/helix/games?`;
 			let queryParams = "";
 			gameID.map(id => {
 				return (queryParams = queryParams + `id=${id}&`);
 			});
 
+			//finalURL of all the game ids appended
 			let finalURL = baseURL + queryParams;
-			console.log(finalURL);
+			// console.log(finalURL);
 
-			setChannels(finalArray);
+			// second api call to get game information by game id
+			let gameNames = await api.get(finalURL);
+			// console.log(gameNames);
+
+			//array of game information
+			let gameNamesArray = gameNames.data.data;
+			console.log(gameNamesArray);
+
+			let finalArray = dataArray.map(stream => {
+				stream.gameName = "";
+				gameNamesArray.map(name => {
+					if (stream.game_id === name.id) {
+						return (stream.gameName = name.name);
+					}
+				});
+				console.log(stream.gameName);
+			});
+
+			// setChannels(finalArray);
 		};
 		fetchData();
 	}, []);
